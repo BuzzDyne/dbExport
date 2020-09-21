40e0e26a1f0971b7c922f2cd8720bf39e7b8fdc4
BuzzDyne <buzzdyne@gmail.com>
2020-09-21 21:46:35 +0700

    Main App and Firestore Module Seperation

---
+++ devApp.py
@@ -0,0 +1,135 @@
+import fsModule
+
+class DevApp:
+    """This class serves as the interface of fsModule (CLI Input)"""
+    def __init__(self):
+        self.fs = fsModule.FsModule()
+        self.companyRef     = ''
+        self.factoryRef     = ''
+        self.prodLineRef    = ''
+        self.machineRef     = ''
+        self.sensorRef      = ''
+
+    def setupNewCompany(self):
+        """A tool to create a new company and all its sublevels"""
+
+        print("--- Welcome to Vibsense Company Creator ---\n")
+
+        # Company
+        cName = input("Please Input Your Company Name: \n")
+        cRef = self.fs.createCompany(cName)
+        print("Created company {} ({})\n".format(cName, cRef))
+
+        # Factory
+        fName = input("Please Input Factory Name: \n")
+        fRef = self.fs.createFactory(cRef, fName)
+        print("Created factory {} ({})\n".format(fName, fRef))
+
+        # ProdLine
+        pName = input("Please Input ProdLine Name: \n")
+        pRef = self.fs.createProdLine(cRef, fRef, pName)
+        print("Created ProdLine {} ({})\n".format(pName, pRef))
+
+        # Machine
+        mName = input("Please Input Machine Name: \n")
+        mRef = self.fs.createMachine(cRef, fRef, pRef, mName)
+        print("Created Machine {} ({})\n".format(mName, mRef))
+
+        # Sensor
+        sName   = input("Please Input Sensor Name: \n")
+        sTag    = input("Please Input Sensor Hexadecimal Tag: \n")
+        sRef = self.fs.createSensor(cRef,fRef, pRef, mRef, sName, sTag)
+        print("Created Sensor {} ({})\n".format(sName, sRef))
+
+        # SensorData
+        confirmInput = input("Do you want to add data to {} ({}) ? (y/n)".format(sName, sRef))
+        if(confirmInput == 'y'):
+            dataSrc = input("Please input the data source filename (csv): \n")
+            self.addSensorDataRows(sRef,dataSrc)
+
+    def addSensorDataRows(self, sensorID, dataFileName):
+      """Pushes multiple data-rows of a sensor (given the docID)"""
+      dataRows = self.parseSensorData(dataFileName)
+
+      self.fs.addSensorDataRows(sensorID,dataRows)
+
+    def addSensorDataRow(self, sensorID, sensData):
+        """Pushes a single data-row of a sensor (given the DocID)"""
+
+        # sensData example
+        # t = datetime.now(tz=pytz.timezone('Asia/Jakarta'))
+        # sensorData = {
+        #             '_timestamp': t,
+        #             'batt': 3.333,
+        #             'peak_x': 5.555,
+        #             'peak_y': 4.444,
+        #             'peak_z': 8.888,
+        #             'rms_x': 2.222,
+        #             'rms_y': 2.222,
+        #             'rms_z': 2.222,
+        #             'temp': 66.66,
+        #             'fft':None
+        #         }
+        self.fs.addSensorDataRow(sensorID,sensData,True)
+
+    # Helper function
+
+    def parseSensorData(self, fileName):
+        """Take SensorData(.csv) file location as param and returns a list of dicts of SensorData"""
+
+        #    0         1       2      3     4       5        6      7      8      9 
+        # sens_id, timestamp, batt, temp, x_peak, y_peak, z_peak, x_rms, y_rms, z_rms
+        fields = []
+        rows = []
+
+        result = []
+
+        with open(fileName, 'r') as csvfile: 
+            csvreader = csv.reader(csvfile) 
+            
+            fields = next(csvreader) 
+        
+            for row in csvreader: 
+                rows.append(row)
+      
+        for row in rows:
+            data = {
+                u'_timestamp': datetime.strptime(row[1], '%m/%d/%Y %H:%M'),
+                u'batt': float(row[2]),
+                u'peak_x': float(row[4]),
+                u'peak_y': float(row[5]),
+                u'peak_z': float(row[6]),
+                u'rms_x': float(row[7]),
+                u'rms_y': float(row[8]),
+                u'rms_z': float(row[9]),
+                u'temp': float(row[3]),
+                u'fft': None
+            }
+
+            result.append(data)
+
+        # print(result)
+
+        return result
+
+
+dev = DevApp()
+
+# sensData example
+    # t = datetime.now(tz=pytz.timezone('Asia/Jakarta'))
+    # sensorData = {
+    #             '_timestamp': t,
+    #             'batt': 3.333,
+    #             'peak_x': 5.555,
+    #             'peak_y': 4.444,
+    #             'peak_z': 8.888,
+    #             'rms_x': 2.222,
+    #             'rms_y': 2.222,
+    #             'rms_z': 2.222,
+    #             'temp': 66.66,
+    #             'fft':None
+    #         }
+
+# dev.addSensorDataRow("lOWj7bWGrMtxhw9hwvJo", sensorData)
+
+dev.setupNewCompany()
--- fsModule.py
+++ fsModule.py
@@ -1,7 +1,6 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-from firebase_admin.firestore import SERVER_TIMESTAMP
 
 from datetime import datetime
 import pytz
@@ -9,28 +8,28 @@ import pytz
 import csv
 import sys
 
-# Update Machine's sensors metadata
-# [V] Added MachineParentDocID when creating new sensor (createSensor)
-# [V] createSensor will only create an initial metadata on ParentMachine doc.
-# [V] Implemented getSensorParentDocID
-# [V] Update machine's sensor metadata when adding new sensor data (addSensorDataRow)
-# [ ] Create a functionality to update sensor metadata based on latest existing data (refreshSensorMetadatas)
+# Notes
+    # Update Machine's sensors metadata
+    # [V] Added MachineParentDocID when creating new sensor (createSensor)
+    # [V] createSensor will only create an initial metadata on ParentMachine doc.
+    # [V] Implemented getSensorParentDocID
+    # [V] Update machine's sensor metadata when adding new sensor data (addSensorDataRow)
+    # [ ] Create a functionality to update sensor metadata based on latest existing data (refreshSensorMetadatas)
 
-# v3 = changed the document layout for React Integration
-    # All c,f, and p fields name changed to be same as each others
-      # "CompanyName", "FactoryName", "ProdLineName" > "name", etc 
-    # Every child metadata of levels mentioned above is changed to "childrenMetadata" (along with its fields)
-      # "factoryID" -> "childID"
+    # v3 = changed the document layout for React Integration
+        # All c,f, and p fields name changed to be same as each others
+        # "CompanyName", "FactoryName", "ProdLineName" > "name", etc 
+        # Every child metadata of levels mentioned above is changed to "childrenMetadata" (along with its fields)
+        # "factoryID" -> "childID"
 
-# TODOS
-#   - Adding Sensor Data doesnt update the sensorsMetadata field in "Machine" docs
-#   - Currently document in Sensors/ collection can exceed the limit of 1MB per document
-# 
+    # TODOS
+    #   - Adding Sensor Data doesnt update the sensorsMetadata field in "Machine" docs
+    #   - Currently document in Sensors/ collection can exceed the limit of 1MB per document
+    # 
 
-# NOTES
-#   - Push UTC+7 Timezone (pytz) to Firestore, 
-#         If unaware datetime is push, firestore will assume it is UTC+0 
-
+    # NOTES
+    #   - Push UTC+7 Timezone (pytz) to Firestore, 
+    #         If unaware datetime is push, firestore will assume it is UTC+0 
 
 cred = credentials.Certificate("vibsensedup-firebase-adminsdk-uzlnm-c655ba999f.json")
 
@@ -190,7 +189,7 @@ class FsModule:
     def getSensorParentDocID(self, sensorID):
         """Returns a complete address of the given sensor's MachineParentDocID from 'Sensors' collection."""
 
-        resDict = fa.fm.readDocument("Sensors/{}".format(sensorID))
+        resDict = fa.fs.readDocument("Sensors/{}".format(sensorID))
 
         return resDict['ParentMachineDocID']
 
@@ -424,137 +423,3 @@ class FsModule:
                 break
             
         return None
-
-class FsApp:
-    """This class serves as the interface of fsModule (CLI Input)"""
-    def __init__(self):
-        self.fm = FsModule()
-        self.companyRef     = ''
-        self.factoryRef     = ''
-        self.prodLineRef    = ''
-        self.machineRef     = ''
-        self.sensorRef      = ''
-
-    def setupNewCompany(self):
-        """A tool to create a new company and all its sublevels"""
-
-        print("--- Welcome to Vibsense Company Creator ---\n")
-
-        # Company
-        cName = input("Please Input Your Company Name: \n")
-        cRef = self.fm.createCompany(cName)
-        print("Created company {} ({})\n".format(cName, cRef))
-
-        # Factory
-        fName = input("Please Input Factory Name: \n")
-        fRef = self.fm.createFactory(cRef, fName)
-        print("Created factory {} ({})\n".format(fName, fRef))
-
-        # ProdLine
-        pName = input("Please Input ProdLine Name: \n")
-        pRef = self.fm.createProdLine(cRef, fRef, pName)
-        print("Created ProdLine {} ({})\n".format(pName, pRef))
-
-        # Machine
-        mName = input("Please Input Machine Name: \n")
-        mRef = self.fm.createMachine(cRef, fRef, pRef, mName)
-        print("Created Machine {} ({})\n".format(mName, mRef))
-
-        # Sensor
-        sName   = input("Please Input Sensor Name: \n")
-        sTag    = input("Please Input Sensor Hexadecimal Tag: \n")
-        sRef = self.fm.createSensor(cRef,fRef, pRef, mRef, sName, sTag)
-        print("Created Sensor {} ({})\n".format(sName, sRef))
-
-        # SensorData
-        confirmInput = input("Do you want to add data to {} ({}) ? (y/n)".format(sName, sRef))
-        if(confirmInput == 'y'):
-            dataSrc = input("Please input the data source filename (csv): \n")
-            self.addSensorDataRows(sRef,dataSrc)
-
-    def addSensorDataRows(self, sensorID, dataFileName):
-      """Pushes multiple data-rows of a sensor (given the docID)"""
-      dataRows = self.parseSensorData(dataFileName)
-
-      self.fm.addSensorDataRows(sensorID,dataRows)
-
-    def addSensorDataRow(self, sensorID, sensData):
-        """Pushes a single data-row of a sensor (given the DocID)"""
-
-        # sensData example
-        # t = datetime.now(tz=pytz.timezone('Asia/Jakarta'))
-        # sensorData = {
-        #             '_timestamp': t,
-        #             'batt': 3.333,
-        #             'peak_x': 5.555,
-        #             'peak_y': 4.444,
-        #             'peak_z': 8.888,
-        #             'rms_x': 2.222,
-        #             'rms_y': 2.222,
-        #             'rms_z': 2.222,
-        #             'temp': 66.66,
-        #             'fft':None
-        #         }
-        self.fm.addSensorDataRow(sensorID,sensData,True)
-
-    # Helper function
-
-    def parseSensorData(self, fileName):
-        """Take SensorData(.csv) file location as param and returns a list of dicts of SensorData"""
-
-        #    0         1       2      3     4       5        6      7      8      9 
-        # sens_id, timestamp, batt, temp, x_peak, y_peak, z_peak, x_rms, y_rms, z_rms
-        fields = []
-        rows = []
-
-        result = []
-
-        with open(fileName, 'r') as csvfile: 
-            csvreader = csv.reader(csvfile) 
-            
-            fields = next(csvreader) 
-        
-            for row in csvreader: 
-                rows.append(row)
-      
-        for row in rows:
-            data = {
-                u'_timestamp': datetime.strptime(row[1], '%m/%d/%Y %H:%M'),
-                u'batt': float(row[2]),
-                u'peak_x': float(row[4]),
-                u'peak_y': float(row[5]),
-                u'peak_z': float(row[6]),
-                u'rms_x': float(row[7]),
-                u'rms_y': float(row[8]),
-                u'rms_z': float(row[9]),
-                u'temp': float(row[3]),
-                u'fft': None
-            }
-
-            result.append(data)
-
-        # print(result)
-
-        return result
-
-
-fa = FsApp()
-
-# sensData example
-    # t = datetime.now(tz=pytz.timezone('Asia/Jakarta'))
-    # sensorData = {
-    #             '_timestamp': t,
-    #             'batt': 3.333,
-    #             'peak_x': 5.555,
-    #             'peak_y': 4.444,
-    #             'peak_z': 8.888,
-    #             'rms_x': 2.222,
-    #             'rms_y': 2.222,
-    #             'rms_z': 2.222,
-    #             'temp': 66.66,
-    #             'fft':None
-    #         }
-
-# fa.addSensorDataRow("lOWj7bWGrMtxhw9hwvJo", sensorData)
-
-# fa.setupNewCompany()
